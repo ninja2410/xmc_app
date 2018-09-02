c489c66282c6be8c0d5099706b8df25d9098e0c3
ninja2410 <pablo.felg1996@gmail.com>
2018-09-02 12:34:57 -0500

    Inicio proyecto XMC

---
+++ Conexion/conexion.php
@@ -0,0 +1,20 @@
+<?php
+class conexion
+{
+	private $servidor='localhost';
+	private $usuario="root";
+	private $pass="database";
+	private $bd="db_xelajumc";
+	public  $objetoconexion;
+
+	public function conectar()
+	{
+
+		$this->obgetoconexion= mysqli_connect($this->servidor,$this->usuario,$this->pass,$this->bd) or die ("error en conexion");
+	}
+	public function desconectar()
+	{
+		mysqli_close($this->obgetoconexion);
+	}
+}
+?>
---
+++ Negocio/ClassAutor.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('..\Biblioteca\Conexion\conexion.php');
+
+class autor
+{
+	public function get()
+	{
+		$bd= new conexion();
+		$bd->conectar();
+		$consultasql="SELECT * FROM autor";
+		$dt = mysqli_query($bd->obgetoconexion,$consultasql);
+		$bd->desconectar();
+		return $dt;
+	}
+	
+	public function set($datos=array())
+	{
+		$db = new conexion();
+		$db->conectar();
+		 foreach ($datos as $campo=>$valor){                     
+		 	$$campo = $valor;                 
+		 }
+		$cadena = "INSERT INTO autor(nombres,fecha_nac) 
+					VALUES('$nombres','$fecha_nac')";	 
+		$consulta = mysqli_query($db->obgetoconexion,$cadena);
+		$db->desconectar();
+		echo "<script type=\"text/javascript\">alert(\"Registro Agregado\");</script>";
+	}
+}
+?>
