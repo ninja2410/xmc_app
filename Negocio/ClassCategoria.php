3a455d3b6f288060a45be46077174a0af301eb9b
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-09-10 15:24:44 -0600

    Clase categoria

---
+++ Negocio/ClassCategoria.php
@@ -0,0 +1,48 @@
+<?php
+require_once('..\..\Conexion\conexion.php');
+/**
+ * MEDICO DE JUGADORES
+ */
+class Categoria
+{
+
+  private $query;
+  public function insert($nombre, $descripcion){
+    $query="CALL SP_CATEGORIA_INSERT('$nombre', '$descripcion');";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function update($id, $nombre, $descripcion,
+   $estado){
+    $query="CALL SP_CATEGORIA_UPDATE($id,'$nombre', '$descripcion', '$estado');";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function delete($id){
+    $query="CALL SP_CATEGORIA_DELETE($id);";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function select($id){
+    $conexion=new conexion();
+    $conexion->conectar();
+    if ($id==-1) {
+      $query="SELECT * FROM categoria WHERE estado=1";
+      $dt=mysqli_query($conexion->objetoconexion,$query);
+    }
+    else{
+      $query="SELECT * FROM categoria WHERE idcategoria=$id AND estado=1";
+      $tmp=mysqli_query($conexion->objetoconexion,$query);
+      $dt=mysqli_fetch_assoc($tmp);
+    }
+    $conexion->desconectar();
+    return $dt;
+  }
+}
+ ?>
