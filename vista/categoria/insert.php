f1ab5d2e68ffd4d571b93f638c47096a50b6449f
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-09-10 16:40:41 -0600

    Insert de categoría

---
+++ vista/categoria/insert.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Categoría - Insertar</title>
+    <?php include '..\layoults\headers2.php'; ?>
+  </head>
+  <body>
+    <?php include '..\layoults\barnav.php'; ?>
+    <div class="content">
+      <div class="col-md-8">
+        <div class="card">
+          <div class="card-header card-header-primary">
+            <h4 class="card-title">INSERTAR CATEGORÍA</h4>
+            <p class="card-category">Complete los campos siguientes</p>
+          </div>
+          <div class="card-body">
+            <form method="post", action="..\categoria\store.php">
+              <input type="hidden" name="operation" value="1">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="bmd-label-floating">Nombre</label>
+                    <input type="text" class="form-control" name="nombre">
+                  </div>
+                </div>
+                </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="bmd-label-floating">Descripcion</label>
+                    <input type="text" class="form-control" name="descripcion">
+                  </div>
+                </div>
+                </div>
+
+              <?php include '..\layoults\botones.php'; ?>
+              <div class="clearfix"></div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php include '..\layoults\footer.php'; ?>
+    <?php include '..\layoults\scripts2.php'; ?>
+  </body>
+</html>
