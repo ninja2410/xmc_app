f744f2ca7a225c2716cc1bc8b49e139eae266d50
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-09-10 16:43:08 -0600

    Clase de prensa

---
+++ Negocio/ClassPrensa.php
@@ -0,0 +1,48 @@
+<?php
+require_once('..\..\Conexion\conexion.php');
+/**
+ * MEDICO DE JUGADORES
+ */
+class Prensa
+{
+
+  private $query;
+  public function insert($nombre, $apellido, $telefono, $empresa){
+    $query="CALL SP_PRENSA_INSERT('$nombre', '$apellido','$telefono', '$empresa');";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function update($id, $nombre, $apellido,
+  $telefono, $empresa){
+    $query="CALL SP_PRENSA_UPDATE($id,'$nombre','$apellido','$telefono', '$empresa', '$estado');";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function delete($id){
+    $query="CALL SP_PRENSA_DELETE($id);";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+		return $dt;
+  }
+
+  public function select($id){
+    $conexion=new conexion();
+    $conexion->conectar();
+    if ($id==-1) {
+      $query="SELECT * FROM prensa WHERE estado=1";
+      $dt=mysqli_query($conexion->objetoconexion,$query);
+    }
+    else{
+      $query="SELECT * FROM prensa WHERE idprensa=$id AND estado=1";
+      $tmp=mysqli_query($conexion->objetoconexion,$query);
+      $dt=mysqli_fetch_assoc($tmp);
+    }
+    $conexion->desconectar();
+    return $dt;
+  }
+}
+ ?>
