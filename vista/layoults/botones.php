01d10e332d951b9dcf8aad3c7b4bdff92d1ca68f
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-09-24 12:43:40 -0600

    Botones redondos ahora, y color verde en aceptar

--- vista/layoults/botones.php
+++ vista/layoults/botones.php
@@ -1,2 +1,2 @@
-<button type="submit" class="btn btn-primary pull-right">Aceptar</button>
-<a href="index.php"> <button type="button" class="btn btn-info pull-right">Regresar</button></a>
+<button type="submit" class="btn btn-success pull-right btn-round">Aceptar</button>
+<a href="index.php"> <button type="button" class="btn btn-info pull-right btn-round">Regresar</button></a>
