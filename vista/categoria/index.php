d92ff1e68f988c1c173e4a5bef9933781f1cde9f
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-09-24 13:03:09 -0600

    Actualizaciones en diseño de categoría. Botón de agregar se ve más responsivo

--- vista/categoria/index.php
+++ vista/categoria/index.php
@@ -2,7 +2,6 @@
 require_once('..\..\Negocio/ClassCategoria.php');
 $categoria=new Categoria();
 $data=$categoria->select(-1);
-
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -11,25 +10,25 @@ $data=$categoria->select(-1);
     <title>Categoría - Listar</title>
     <?php include '..\layoults\headers2.php'; ?>
   </head>
-  <body>
-    <?php
-    include '..\layoults\barnav.php';
-    ?>
+  <body class="profile-page sidebar-collapse">
+    <?php include '..\layoults\barnav.php'; ?>
+
+   <div class="main main-raised">
     <div class="content">
       <div class="container-fluid">
-        <div class="row">
+       
           <div class="col-md-12">
             <div class="card">
-              <div class="card-header card-header-primary">
+              <div class="card-header card-header-danger">
                 <div class="col-lg-10" style="float:left;">
                   <h2 class="card-title ">Categoría</h4>
                   <p class="card-category"> Listado de categorías</p>
                 </div>
                 <div class="col-lg-1" style="float:left">
-                  <a href="..\..\vista\categoria/insert.php" title="Agregar nueva Categoría">
-                    <div class="card-header card-header-success card-header-icon" style="float:left">
-                      <div class="card-icon">
-                        <i class="material-icons">add</i>
+                  <a href="..\..\vista\categoria/insert.php" title="Agregar nueva Categoría" >
+                    <div class="card-header card-header-success card-header-icon" style="float:right">
+                      <div class="card-icon" >
+                        <i class="material-icons" >add</i>
                       </div>
                     </div>
                   </a>
@@ -38,7 +37,7 @@ $data=$categoria->select(-1);
               <div class="card-body">
                 <div class="table-responsive">
                   <table class="table">
-                    <thead class=" text-primary">
+                    <thead class=" text-muted">
                       <th>
                         ID
                       </th>
@@ -55,7 +54,7 @@ $data=$categoria->select(-1);
                        ?>
                       <tr>
                         <td>
-                          <?php echo $row['idcategoria']; ?>
+                          <?php echo $row['id_categoria']; ?>
                         </td>
                         <td>
                           <?php echo $row['nombre']; ?>
@@ -65,7 +64,7 @@ $data=$categoria->select(-1);
                         </td>
                         <td class="td-actions text-left">
                             <div style="float:left">
-                              <a href="..\..\vista\categoria/update.php?id=<?php echo $row['idcategoria']; ?>">
+                              <a href="..\..\vista\categoria/update.php?id=<?php echo $row['id_categoria']; ?>">
                                 <button type="button" rel="tooltip" title="Editar Categoria" class="btn btn-primary btn-link btn-sm">
                                   <i class="material-icons">edit</i>
                                 </button>
@@ -74,7 +73,7 @@ $data=$categoria->select(-1);
                             <div  style="float:left">
                               <form class="" action="..\..\vista\categoria/store.php" method="post">
                                 <input type="hidden" name="operation" value="3">
-                                <input type="hidden" name="id" value="<?php echo $row['idcategoria']; ?>">
+                                <input type="hidden" name="id" value="<?php echo $row['id_categoria']; ?>">
                                 <button type="submit" rel="tooltip" title="Eliminar Categoria" class="btn btn-danger btn-link btn-sm">
                                   <i class="material-icons">close</i>
                                 </button>
@@ -89,11 +88,12 @@ $data=$categoria->select(-1);
                 </div>
               </div>
             </div>
-          </div>
+            
         </div>
       </div>
     </div>
+   </div>
     <?php include '..\layoults\footer.php'; ?>
     <?php include '..\layoults\scripts2.php'; ?>
-  </body>
+</body>
 </html>
--- vista/categoria/insert.php
+++ vista/categoria/insert.php
@@ -5,12 +5,13 @@
     <title>Categoría - Insertar</title>
     <?php include '..\layoults\headers2.php'; ?>
   </head>
-  <body>
+  <body class="profile-page sidebar-collapse">
     <?php include '..\layoults\barnav.php'; ?>
+    <div class="main main-raised">
     <div class="content">
-      <div class="col-md-8">
+      <div class="col-md-12">
         <div class="card">
-          <div class="card-header card-header-primary">
+          <div class="card-header card-header-danger">
             <h4 class="card-title">INSERTAR CATEGORÍA</h4>
             <p class="card-category">Complete los campos siguientes</p>
           </div>
@@ -20,16 +21,16 @@
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label class="bmd-label-floating">Nombre</label>
-                    <input type="text" class="form-control" name="nombre">
+                    <label >Nombre</label>
+                    <input type="text" class="form-control" name="nombre" placeholder="Sub 15">
                   </div>
                 </div>
                 </div>
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label class="bmd-label-floating">Descripcion</label>
-                    <input type="text" class="form-control" name="descripcion">
+                    <label>Descripción</label>
+                    <input type="text" class="form-control" name="descripcion" placeholder="Jugadores en la edad de ...">
                   </div>
                 </div>
                 </div>
@@ -79,5 +80,6 @@
       })
     });
     </script>
+     </div>
   </body>
 </html>
