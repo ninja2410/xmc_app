c031157b40f43eb42ec3f52fb384277ba3e28fd8
Keviin97 <kevinxela13@gmail.com>
2018-09-30 20:17:23 -0600

    Actualizacion - Ficha Medica

---
+++ vista/ficha_medica/detalle.php
@@ -0,0 +1,58 @@
+<div class="container">
+    <ul class="nav nav-pills nav-pills-info justify-content-center">
+        <li class="nav-item"><a class="nav-link active" href="#pill1" data-toggle="tab">Signos vitales</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill2" data-toggle="tab">Criometría/Antropometria</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill3" data-toggle="tab">Evaluación Rodilla</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill4" data-toggle="tab">Evaluación Tobillo</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill5" data-toggle="tab">Meniscos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill6" data-toggle="tab">Músculos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill7" data-toggle="tab">Alineamiento Postular</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill8" data-toggle="tab">Cuello</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill9" data-toggle="tab">Pecho</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill10" data-toggle="tab">Subescapular</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill11" data-toggle="tab">Supraespinal</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill12" data-toggle="tab">Abdominal</a></li>
+        <li class="nav-item"><a class="nav-link" href="#pill13" data-toggle="tab">Otra</a></li>
+    </ul>
+    <div class="tab-content tab-space">
+        <div class="tab-pane active" id="pill1">
+            1
+        </div>
+        <div class="tab-pane" id="pill2">
+           2
+        </div>
+        <div class="tab-pane" id="pill3">
+           3
+        </div>
+        <div class="tab-pane" id="pill4">
+           4
+        </div>
+        <div class="tab-pane" id="pill5">
+            5
+        </div>
+        <div class="tab-pane" id="pill6">
+            6
+        </div>
+        <div class="tab-pane" id="pill7">
+            7
+        </div>
+        <div class="tab-pane" id="pill8">
+            8
+        </div>
+        <div class="tab-pane" id="pill9">
+            9
+        </div>
+        <div class="tab-pane" id="pill10">
+            10
+        </div>
+        <div class="tab-pane" id="pill11">
+            11
+        </div>
+        <div class="tab-pane" id="pill12">
+            12
+        </div>
+        <div class="tab-pane" id="pill13">
+            13
+        </div>
+    </div>
+</div>
--- vista/ficha_medica/insert.php
+++ vista/ficha_medica/insert.php
@@ -12,72 +12,81 @@
       <div class="row">
           <div class="col-md-12">
             <div class="card">
-              <div class="card-header card-header-danger row">
-                <div class="col-md-10">
-                  <h3 class="card-title">Ficha Medica</h3>
-                  <p class="category">Nueva ficha medica.</p>
-                </div>
+              <div class="card-header card-header-danger">
+                    <h3 class="card-title">Ficha Medica</h3>
+                    <p class="category">Nueva ficha medica.</p>
               </div>
-              <div class="card-body text-center table-responsive">
-                <ul class="nav nav-pills nav-pills-success">
+              <div class="card-body text-center">
+                <ul class="nav nav-pills nav-pills-success justify-content-center">
                     <li class="nav-item"><a class="nav-link active" href="#encabezado" data-toggle="tab">Encabezado</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#pill2" data-toggle="tab">Detalle</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#pill3" data-toggle="tab">Options</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#detalle" data-toggle="tab">Detalle 1</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Detalle 2</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="insert.php#signos">Signos Vitales</a>
+                            <a class="dropdown-item" href="#">Criometría/Antropometria</a>
+                            <a class="dropdown-item" href="#">Evaluación Rodilla</a>
+                        </div>
+                    </li>
+                    <li class="nav-item"><a class="nav-link" href="#otro" data-toggle="tab">Otro</a></li>
                 </ul>
                 <div class="tab-content tab-space">
-                    <div class="tab-pane active" id="pill1">
-                    <form method="post", action="..\ficha_medica\store.php" id="frm_fichaMedica">
-                        <input type="hidden" name="operation" value="1">
-                        <div class="form-row"> 
-                            <div class="form-group col-md-4">
-                                <label>Fecha</label>
-                                <input name="fecha" type="date" class="form-control" >
-                            </div>
+                    <div class="tab-pane active" id="encabezado">
+                        <form method="post", action="..\ficha_medica\store.php" id="frm_fichaMedica">
+                            <input type="hidden" name="operation" value="1">
+                            <div class="form-row"> 
+                                <div class="form-group col-md-4">
+                                    <label>Fecha</label>
+                                    <input name="fecha" type="date" class="form-control" >
+                                </div>
 
-                            <div class="form-group col-md-4">
-                                <label>Jugador</label>
-                                <select class="form-control" name="jugador">
-                                    <option selected>Elija un jugador...</option>
-                                    <?php
-                                        include_once('..\..\Negocio/ClassJugador.php');
-                                        $jugador=new Jugador();
-                                        $data=$jugador->select(-1);	
-                                        while ($row = mysqli_fetch_array($data))
-                                        {
-                                            $valor = $row['id_jugador'];
-                                            $texto2 = $row['nombre'];
-                                            $texto = $texto2.' '.$row['apellido'];
-                                            echo '<option value="'.$valor.'">'.$texto.'</option>';
-                                        }
-                                    ?>
-                                </select>
-                            </div>
+                                <div class="form-group col-md-4">
+                                    <label>Jugador</label>
+                                    <select class="form-control" name="jugador">
+                                        <option selected>Elija un jugador...</option>
+                                        <?php
+                                            include_once('..\..\Negocio/ClassJugador.php');
+                                            $jugador=new Jugador();
+                                            $data=$jugador->select(-1);	
+                                            while ($row = mysqli_fetch_array($data))
+                                            {
+                                                $valor = $row['id_jugador'];
+                                                $texto2 = $row['nombre'];
+                                                $texto = $texto2.' '.$row['apellido'];
+                                                echo '<option value="'.$valor.'">'.$texto.'</option>';
+                                            }
+                                        ?>
+                                    </select>
+                                </div>
 
-                            <div class="form-group col-md-4">
-                                <label for="grasa">Grasa</label>
-                                <input type="number" class="form-control" name="grasa">
+                                <div class="form-group col-md-4">
+                                    <label for="grasa">Grasa</label>
+                                    <input type="number" class="form-control" name="grasa">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="form-group col-md-4">
-                                <label for="peso">Peso</label>
-                                <input type="number" class="form-control" name="peso">
-                            </div>
+                            <div class="form-row">
+                                <div class="form-group col-md-4">
+                                    <label for="peso">Peso</label>
+                                    <input type="number" class="form-control" name="peso">
+                                </div>
 
-                            <div class="form-group col-md-4">
-                                <label for="talla">Talla</label>
-                                <input type="number" class="form-control" name="talla">
+                                <div class="form-group col-md-4">
+                                    <label for="talla">Talla</label>
+                                    <input type="number" class="form-control" name="talla">
+                                </div>
                             </div>
-                        </div>
-                        <?php include '..\layoults\botones.php'; ?>
-                        <div class="clearfix"></div>
-                    </form>
+                            <?php include '..\layoults\botones.php'; ?>
+                            <div class="clearfix"></div>
+                        </form>
                     </div>
-                    <div class="tab-pane" id="pill2">
-                        
+                    <div class="tab-pane" id="detalle">
+                        <?php include 'detalle.php'?>
                     </div>
-                    <div class="tab-pane" id="pill3">
-                        
+                    <div class="tab-pane" id="otro">
+                        Otro
+                    </div>
+                    <div class="tab-pane" id="signos">
+                        Signos
                     </div>
                 </div>
               </div>
