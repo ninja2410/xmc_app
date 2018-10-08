1ddfe0f6d574f160dfde5978f346b10b9d843332
Keviin97 <kevinxela13@gmail.com>
2018-10-07 22:46:49 -0600

    Login - Plantilla

---
+++ vista/layoults/barnav3.php
@@ -0,0 +1,41 @@
+<nav class="navbar navbar-default navbar-expand-lg">
+<div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="Index.php">
+          <img src="../assets/img/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+          Xelajú MC
+        </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+
+      <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php"><i class="fas fa-home fa-lg"></i> Inicio</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/Xelaju_Oficial" target="_blank" data-original-title="Siguenos en Twitter">
+                <i class="fab fa-twitter fa-lg"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/Clubxelajumcoficial/" target="_blank" data-original-title="Siguenos en Facebook">
+                <i class="fab fa-facebook-f fa-lg"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/csdxelajumc/" target="_blank" data-original-title="Siguenos en Instagram">
+                <i class="fab fa-instagram fa-lg"></i>
+              </a>
+            </li>
+          </ul>
+      </div>
+  </div>
+  <!-- /.container-->
+</nav>
---
+++ vista/login/login.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Inicio</title>
+    <?php include '..\layoults\headers2.php'; ?>
+</head>
+<body class="login-page sidebar-collapse">
+<style>
+        .fondoP {
+            background-image:url(https://www.guatemala.com/fotos/201511/xelaju-808x500.jpg);
+            max-width: 100%;
+            height: 92%;
+            position:relative;
+            color: white;
+            z-index:1;
+        }
+
+        .titulo{
+            font-weight: 700;
+            font-family: "Roboto Slab", "Times New Roman", serif;
+        }
+
+        .fondoP::after {
+            content:"";
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            background:rgba(0,0,0,0.6);
+            z-index:-1;
+        }
+
+        .fondo {
+            background-image:url(https://www.guatemala.com/fotos/201511/xelaju-808x500.jpg);
+            max-width: 100%;
+            height: 90%;
+            position:relative;
+            color: white;
+            z-index:1;
+        }
+
+        .fondo::after {
+            content:"";
+            position:absolute;
+            top:0;
+            left:0;
+            width:100%;
+            height:100%;
+            background:rgba(0,0,0,0.6);
+            z-index:-1;
+        }
+        
+        .rojo{
+            background-color:#d82023 ;
+        }
+
+        .azul {
+            background-color: #294482;
+            -webkit-transition-duration: 0.4s; /* Safari */
+            transition-duration: 0.4s;
+        }
+        .azul:hover {
+            background-color: #294482;
+            color: white;
+        }
+    </style>
+  <div class="page-header header-filter" style="background-image: url('../assets/img/login.jpg'); background-size: cover; background-position: top center;">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4 col-md-6 ml-auto mr-auto">
+          <div class="card card-login">
+            <form class="form" method="" action="">
+                <br>
+              <div class="card-header card-header-primary text-center">
+                <h3 class="card-title">Login</h3>
+              </div>
+              <div class="card-body">
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fas fa-user fa-lg"></i>
+                    </span>
+                  </div>
+                  <input type="email" class="form-control" placeholder="Nombre de usuario...">
+                </div>
+                <div class="input-group">
+                  <div class="input-group-prepend">
+                    <span class="input-group-text">
+                        <i class="fas fa-lock fa-lg"></i>
+                    </span>
+                  </div>
+                  <input type="password" class="form-control" placeholder="Contraseña...">
+                </div>
+              </div>
+              <div class="footer text-center">
+                <a href="#pablo" class="btn azul btn-wd btn-lg">Ingresar</a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <?php include '..\layoults\footer.php'; ?>
+    <?php include '..\layoults\scripts2.php'; ?>
+</body>
+
+</html>
