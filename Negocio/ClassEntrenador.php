f53f8ec08ba428b08acbb696de220f8df2445c15
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-10-10 01:16:07 -0600

    Clase Entrenador actualizado

--- Negocio/ClassEntrenador.php
+++ Negocio/ClassEntrenador.php
@@ -7,18 +7,17 @@ class Entrenador
 {
 
   private $query;
-  public function insert($nombre, $apellido, $fecha_nacimiento, $fecha_inicio, $fecha_fin, $telefono, $direccion, $id_tipo_entrenador){
+  public function insert($nombre, $apellido, $fecha_nacimiento, $fecha_inicio, $fecha_fin, $telefono, $direccion,$foto,$nacionalidad, $id_contrato){
     $query="CALL SP_ENTRENADOR_INSERT('$nombre', '$apellido', '$fecha_nacimiento', '$fecha_inicio',
-    '$fecha_fin', $telefono, '$direccion', $id_tipo_entrenador);";
+    '$fecha_fin', '$telefono', '$direccion','$foto','$nacionalidad', $id_contrato);";
     $bd= new conexion();
 		$dt=$bd->execute_query($query);
 		return $dt;
   }
 
-  public function update($id, $nombre, $apellido, $f_nacimiento, $f_inicio, $f_fin,
-   $estado, $telefono, $direccion, $id_tipo_entrenador){
-    $query="CALL SP_ENTRENADOR_UPDATE($id,'$nombre', '$apellido', '$f_nacimiento', '$f_inicio',
-    '$f_fin',$estado,'$telefono', '$direccion',$id_tipo_entrenador);";
+  public function update($id, $nombre, $apellido, $fecha_nacimiento, $fecha_inicio, $fecha_fin, $telefono, $direccion,$foto,$nacionalidad, $id_contrato){
+    $query="CALL SP_ENTRENADOR_UPDATE($id,'$nombre', '$apellido', '$fecha_nacimiento', '$fecha_inicio',
+    '$fecha_fin', '$telefono', '$direccion','$foto','$nacionalidad', $id_contrato);";
     $bd= new conexion();
 		$dt=$bd->execute_query($query);
 		return $dt;
@@ -31,18 +30,25 @@ class Entrenador
 		return $dt;
   }
 
+
+  public function correlativo(){
+    $query="SELECT coalesce(MAX(id_entrenador)+1, 1) as x from ENTRENADOR;";
+    $bd= new conexion();
+		$dt=$bd->execute_query($query);
+    $id=mysqli_fetch_array($dt);
+    return $id[0][0];
+  }
+
+
   public function select($id){
     $conexion=new conexion();
     $conexion->conectar();
     if ($id==-1) {
-      $query = "SELECT id_entrenador,nombre,apellido,fecha_nacimiento,fecha_inicio,fecha_fin,telefono,direccion,
-      TIPO_ENTRENADOR.descripcion FROM ENTRENADOR,TIPO_ENTRENADOR WHERE ENTRENADOR.estado = 1 AND ENTRENADOR.id_tipo_entrenador =
-      TIPO_ENTRENADOR.id_tipo_entrenador";
-      //$query="SELECT * FROM entrenador WHERE estado=1";
+      $query = "SELECT * FROM ENTRENADOR WHERE estado = 1";
       $dt=mysqli_query($conexion->objetoconexion,$query);
     }
     else{
-      $query="SELECT * FROM ENTRENADOR WHERE id_entrenador=$id AND estado=1";
+      $query="SELECT E.nombre,E.apellido,E.fecha_nacimiento,E.fecha_inicio,E.fecha_fin,E.telefono,E.direccion,E.foto,E.nacionalidad,C.id_contrato,C.titulo FROM ENTRENADOR E, CONTRATO C WHERE E.id_entrenador=$id AND E.estado=1 AND E.id_contrato = C.id_contrato;";
       $tmp=mysqli_query($conexion->objetoconexion,$query);
       $dt=mysqli_fetch_assoc($tmp);
     }
