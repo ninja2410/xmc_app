9aca38b353319c486b29ca115281c5cae3da151e
ninja2410 <37285261+ninja2410@users.noreply.github.com>
2018-10-15 18:29:29 -0600

    Correcciones

--- Negocio/ClassJugador.php
+++ Negocio/ClassJugador.php
@@ -44,7 +44,7 @@ class Jugador
       $dt=mysqli_query($conexion->objetoconexion,$query);
     }
     else{
-      $query="SELECT J.id_jugador, J.nombre jnombre, J.direccion, J.fecha_nacimiento, J.estado, J.padre, J.madre, J.telefono, J.procedencia, J.apellido japellido, 
+      $query="SELECT J.id_jugador, J.nombre, J.direccion, J.fecha_nacimiento, J.estado, J.padre, J.madre, J.telefono, J.procedencia, J.apellido,
       J.foto, J.id_posicion, P.descripcion, J.camisola, J.id_contrato,C.nombre as categoria, AC.fecha_inicio, AC.fecha_final, AC.id_asignacion_categoria, CT.titulo
       FROM ASIGNACION_CATEGORIA AC, CATEGORIA C,JUGADOR J, POSICION P, CONTRATO CT
       WHERE J.id_jugador=$id AND AC.estado=1 and C.estado=1 and J.estado=1 and P.estado=1 and CT.estado=1 AND J.id_posicion=P.id_posicion
