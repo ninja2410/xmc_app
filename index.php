2af45123334a7ea06c15ae21cc975eda10330dc3
ninja2410 <pablo.felg1996@gmail.com>
2018-10-28 15:32:46 -0600

    Arreglo despues del merge en documentos de jugador

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-    <?php include '..\XMC_APP\vista\layoults\headers.php'; ?>
-  </head>
-  <body>
-    <?php include '..\XMC_APP\vista\layoults\barnav.php'; ?>
-    <div class="content">
-
-    </div>
-    <?php include '..\XMC_APP\vista\layoults\footer.php'; ?>
-    <?php include '..\XMC_APP\vista\layoults\scripts.php'; ?>
-  </body>
-</html>
+<?php
+header('Location:vista/login/login.php');
+ ?>
--- vista/jugador/documentos.php
+++ vista/jugador/documentos.php
@@ -31,19 +31,9 @@ $player=$jugador->select($_GET['id']);
                     <h4 class="card-title ">Documentos digitales</h4>
                     <p class="card-category"> Listado de documentos almacenados en el sistema asignados al jugador: <?php echo $player['nombre'].' '.$player['apellido']; ?></p>
                   </div>
-<<<<<<< HEAD
-                  <div class="col-lg-1" style="float:left">
-                    <a href="..\..\vista\jugador/create.php?id=<?php echo $_GET['id']; ?>">
-                      <div class="card-header card-header-success card-header-icon" style="float:left">
-                        <div class="card-icon">
-                          <i class="material-icons">add</i>
-                        </div>
-                      </div>
-=======
                   <div class="col-md-2 text-right">
-                    <a href="..\..\vista\documento_digital/insert.php" class="btn btn-success btn-fab btn-fab-mini btn-round btn-lg" role="button" aria-disabled="true" rel="tooltip" title="Agregar documento">
+                    <a href="..\..\vista\jugador/create.php?id=<?php echo $_GET['id']; ?>" class="btn btn-success btn-fab btn-fab-mini btn-round btn-lg" role="button" aria-disabled="true" rel="tooltip" title="Agregar documento">
                     <i class="material-icons">add</i>
->>>>>>> d1b68a1ca2f7b067fa81734d15762a2eaad761ba
                     </a>
                   </div>
                 </div>
