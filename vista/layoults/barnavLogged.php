693261ef92d4e36607f7444d5cc92ae6f809b3c1
Emmanuel Herrera Mull <eehmull@gmail.com>
2018-10-30 11:53:46 -0600

    Barnav

--- vista/layoults/barnavLogged.php
+++ vista/layoults/barnavLogged.php
@@ -117,7 +117,6 @@ foreach($permisos as $key => $value)
                 <a class="dropdown-item" href="..\..\vista\equipo/index.php">Equipos</a>
                 
                 <a class="dropdown-item" href="..\..\vista\estadio/index.php">Estadios</a>
-                <a class="dropdown-item" href="..\..\vista\estadisticaJugador/index.php">Estadísticas</a>
                 <a class="dropdown-item" href="..\..\vista\temporada/index.php">Temporadas</a>
                 <a class="dropdown-item" href="..\..\vista\alineacion/index.php">Alineaciones</a>
                 <a class="dropdown-item" href="..\..\vista\partido/index.php">Registro de Partidos</a>
